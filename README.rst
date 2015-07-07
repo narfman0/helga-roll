3b339937890d1faa78ffb17d5a83dd239221db78
Jon Robison <narfman0@gmail.com>
2015-07-06 22:02:37 -0400

    -Add list choosing to roll command

--- README.rst
+++ README.rst
@@ -6,12 +6,31 @@ Roll dice
 Usage
 -----
 
-``!roll d<sides>`` - rolls a <sides>-sided dice
+``!roll <count>d<sides>+<modifier>`` - rolls <count> <sides>-sided dice with
+modifier
+
+To roll a dice and specify how many sides, place an integer in <sides>
+
+``!roll d6`` - rolls a single d6, printing the output
+
+Can add multiple in fron of d to roll multiples of a particular die. The total
+is given at the end.
+
+``!roll 3d6`` - rolls 3 d6s, then sums them up.
+
+Can add modifiers which will get added to the sum of the rolls
+
+``!roll 3d6+6`` - rolls 3 d6s the same as ``3d6``, but adds 6 at the end
+
+A random decision can be given by giving a comma separated list (surrounded
+by quotation marks)
+
+``!roll "red,blue,green"`` - return a single random item from list, e.g. "red"
 
 
 License
 -------
 
-Copyright (c) 2014 Jon Robison
+Copyright (c) 2015 Jon Robison
 
 See included LICENSE for licensing information
--- helga_roll/plugin.py
+++ helga_roll/plugin.py
@@ -5,21 +5,36 @@ _help_text = 'Roll a dice. \
 Usage: !roll d<sides>'
 _roll_regex = re.compile('(\d+)?d(\d+)\+?(\d+)?')
 
-@command('roll', aliases=['dice'], help=_help_text)
+def roll_dice(count=1, sides=20, modifier=0):
+    """ Perform roll with given parameters """
+    result = ''
+    total = 0
+    for _ in range(0, count):
+        roll = random.randint(1, int(sides))
+        result += str(roll) + ' '
+        total += roll
+    total += modifier
+    result += '= ' + str(total)
+    return result
+
+def parse_roll(match):
+    """ Parse the match to a roll and return string representation """
+    count, sides, modifier = match.groups()
+    count = 1 if not count else int(count)
+    modifier = 0 if not modifier else int(modifier)
+    return roll_dice(count, sides, modifier)
+
+@command('roll', aliases=['dice'], help=_help_text, shlex=True)
 def roll(client, channel, nick, message, cmd, args):
     """ Roll the bones... """
     result = 'Result of '
     for rolls in args:
         result += rolls + ': '
-        count, sides, modifier = re.search(_roll_regex, rolls).groups()
-        count = 1 if not count else int(count)
-        modifier = 0 if not modifier else int(modifier)
-        total = 0
-        for _ in range(0, count):
-            roll = random.randint(1, int(sides))
-            result += str(roll) + ' '
-            total += roll
-        total += modifier
-        result += '= ' + str(total)
+        match = re.search(_roll_regex, rolls)
+        if match:
+            result += parse_roll(match)
+        elif ',' in rolls:
+            roll_list = rolls.split(',')
+            result += random.choice(roll_list)
         result += ', ' if rolls != args[-1] else ' '
     return result
